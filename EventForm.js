c4df0342a96b6e10d46fa025ceac59b894d1213d
AditiSharma21 <147996221+AditiSharma21@users.noreply.github.com>
2024-08-19 01:22:33 +0530

    Create EventForm.js

---
+++ EventForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function EventForm() {
+  return (
+    <div>
+      <h1>Event Form</h1>
+      <p>This is the event form page.</p>
+    </div>
+  );
+}
+
+export default EventForm;

a47d64caa840ac658b8cf8388283cf51aa48001e
AditiSharma21 <147996221+AditiSharma21@users.noreply.github.com>
2024-08-19 01:22:11 +0530

    Create EventDetails.js

---
+++ EventDetails.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function EventDetails() {
+  return (
+    <div>
+      <h1>Event Details</h1>
+      <p>This is the event details page.</p>
+    </div>
+  );
+}
+
+export default EventDetails;

146ab47b9663c028e12c982d13cbf40a40480127
AditiSharma21 <147996221+AditiSharma21@users.noreply.github.com>
2024-08-19 01:24:00 +0530

    Create EventList.js

---
+++ EventList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function EventList() {
+  return (
+    <div>
+      <h1>Event List</h1>
+      <p>This is the event list page.</p>
+    </div>
+  );
+}
+
+export default EventList;

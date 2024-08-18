b5fc0640617d3bab0e2e47df31653b3f8ab4be41
AditiSharma21 <147996221+AditiSharma21@users.noreply.github.com>
2024-08-19 01:24:23 +0530

    Create Filter.js

---
+++ Filter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Filter() {
+  return (
+    <div>
+      <h1>Filter</h1>
+      <p>This is the filter page.</p>
+    </div>
+  );
+}
+
+export default Filter;
